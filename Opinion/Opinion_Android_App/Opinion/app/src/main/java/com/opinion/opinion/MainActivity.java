c9817a2876ebac8f5762b3d0412a119c220d8564
Marcus F <tsbb11stressedblood>
2017-04-23 18:59:46 +0200

    All in order

--- Opinion/Opinion_Android_App/Opinion/app/src/main/java/com/opinion/opinion/MainActivity.java
+++ Opinion/Opinion_Android_App/Opinion/app/src/main/java/com/opinion/opinion/MainActivity.java
@@ -1,13 +1,36 @@
 package com.opinion.opinion;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            final Button button = (Button) findViewById(R.id.buttonRestaurant);
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // Perform action on click
+                    startRestaurantFinder(v);
+                }
+            });
     }
+
+    /** Called when the user taps the Send button */
+    public void startRestaurantFinder(View view) {
+        Intent intent = new Intent(this, restaurantFind.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+
 }
--- Opinion/Opinion_Android_App/Opinion/app/src/main/java/com/opinion/opinion/restaurantFind.java
+++ Opinion/Opinion_Android_App/Opinion/app/src/main/java/com/opinion/opinion/restaurantFind.java
@@ -2,9 +2,8 @@ package com.opinion.opinion;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class restaurantFind extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -20,6 +20,7 @@ public class restaurantFind extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_find);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -40,6 +41,10 @@ public class restaurantFind extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
     }
 
     @Override
